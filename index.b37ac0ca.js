606b4893961a9f30adbc7583d6cf8dd7926fc1ef
GROM-85 <109967972+GROM-85@users.noreply.github.com>
2022-12-21 11:20:53 +0000

    Deploying to gh-pages from @ GROM-85/FlowerShop@946ccd53aca93d6202370c57d5a31829450ec0da 🚀

---
+++ index.b37ac0ca.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".blog__slider-list").slick({slidesToShow:3,centerMode:!0,centerPadding:"60px",adaptiveHeight:!0,variableWidth:!0,speed:900,easing:"ease",autoplay:!0,autoplaySpeed:1e3,initialSlide:1})}));
+//# sourceMappingURL=index.b37ac0ca.js.map
